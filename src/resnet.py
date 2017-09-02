1e860421ccb88a39c0d17070e24dc339ad834904
Shintaro-Indo <shintaro.indo@gmail.com>
2017-09-02 13:29:17 +0900

    fix

--- src/resnet.py
+++ src/resnet.py
@@ -10,18 +10,16 @@ class ResBlock(chainer.Chain):
 
     def __init__(self, in_size, ch):
         super(ResBlock, self).__init__()
-        initialW = initializers.HeNormal()
+        initialW = initializers.HeNormal(
 
-        with self.init_scope():
-            self.conv1 = L.Convolution2D(
-                in_size, ch, 1, 1, 0, initialW=initialW, nobias=True)
-            self.bn1 = L.BatchNormalization(ch)
-            self.conv2 = L.Convolution2D(
-                ch, ch, 3, 1, 1, initialW=initialW, nobias=True)
-            self.bn2 = L.BatchNormalization(ch)
-            self.conv3 = L.Convolution2D(
-                ch, in_size, 1, 1, 0, initialW=initialW, nobias=True)
-            self.bn3 = L.BatchNormalization(in_size)
+        # with self.init_scope():
+        self.conv1 = L.Convolution2D(in_size, ch, 1, 1, 0, initialW=initialW, nobias=True),
+        self.bn1 = L.BatchNormalization(ch),
+        self.conv2 = L.Convolution2D(ch, ch, 3, 1, 1, initialW=initialW, nobias=True),
+        self.bn2 = L.BatchNormalization(ch),
+        self.conv3 = L.Convolution2D(ch, in_size, 1, 1, 0, initialW=initialW, nobias=True),
+        self.bn3 = L.BatchNormalization(in_size),
+        )
 
     def __call__(self, x):
         h = F.relu(self.bn1(self.conv1(x)))
